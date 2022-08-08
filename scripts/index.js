b97834434f65b09dbc21c2dd47918a1ef31e9d2f
Tatyana Karpova <tatyana.pushkareva.1997@mail.ru>
2022-08-09 08:45:30 +1000

    написать js

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,32 @@
 const popup = document.querySelector('.popup');
 const openButton = document.querySelector('.profile__edit-button_open-popup');
 const closeButton = popup.querySelector('.popup__close-icon_close-popup');
+const saveButton = popup.querySelector('.popup__button-save');
 
 const togglePopup = function () {
     popup.classList.toggle('popup_opened');
 }
 
+const closePopup = function () {
+    popup.classList.add('popup__button-save');
+}
+
 openButton.addEventListener('click', togglePopup);
 closeButton.addEventListener('click', togglePopup);
+saveButton.addEventListener('click', closePopup);
 
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__item-name');
-let occupationInput = document.querySelector('.popup__item-occupation');
+let nameInputElement = document.querySelector('.popup__item-name');
+let occupationInputElement = document.querySelector('.popup__item-occupation');
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    nameInput.getAttribute('value');
-    occupationInput.getAttribute('value');
-    const nameInputNew = document.getElementById('name');
-    const occupationInputNew = document.getElementById('occupation');
-    const nameInputAdded = nameInputNew.textContent;
-    const occupationInputAdded = occupationInputNew.textContent;
+    const name = nameInputElement.value;
+    const occupation = occupationInputElement.value;
+    const profileNameElement = document.querySelector('.profile__title');
+    profileNameElement.textContent = name;
+    const profileOccupationElement = document.querySelector('.profile__subtitle');
+    profileOccupationElement.textContent = occupation;
 }
 
 formElement.addEventListener('submit', formSubmitHandler); 
