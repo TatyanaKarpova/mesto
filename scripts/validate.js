01776d9a5b3f9930288bcd5f45946ec57da95649
Tatyana Karpova <tatyana.pushkareva.1997@mail.ru>
2022-09-05 23:20:15 +1000

    feature: начать реализовывать валидацию форм (показ ошибок)

---
+++ scripts/validate.js
@@ -0,0 +1,51 @@
+const validationConfig = {
+    formSelector: '.popup__form',
+    inputSelector: '.popup__item',
+    submitButtonSelector: '.popup__button',
+    inactiveButtonClass: 'popup__button_invalid',
+    inputErrorClass: 'popup__item_type_error',
+    errorClass: 'error_visible'
+};
+
+const showInputError = (formElement, inputElement, errorMessage, config) => {
+    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+    inputElement.classList.add(config.inputErrorClass);
+    errorElement.classList.add(config.errorClass);
+    errorElement.textContent = errorMessage;
+};
+
+const hideInputError = (formElement, inputElement, config) => {
+    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+    inputElement.classList.remove(config.inputErrorClass);
+    errorElement.classList.remove(config.errorClass);
+    errorElement.textContent = '';
+}
+
+const checkInputValidity = (formElement, inputElement, config) => {
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, inputElement.validationMessage, config);
+    } else {
+        hideInputError(formElement, inputElement, config);
+    }
+};
+
+const setEventListeners = (formElement, config) => {
+    const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+    inputList.forEach((inputElement) => {
+        inputElement.addEventListener('input', () => {
+            checkInputValidity(formElement, inputElement, config);
+        });
+    });
+};
+
+const enableValidation = (config) => {
+    const formList = Array.from(document.querySelectorAll(config.formSelector));
+    formList.forEach((formElement) => {
+        formElement.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+        });
+        setEventListeners(formElement, config);
+    });
+};
+
+enableValidation(validationConfig);
